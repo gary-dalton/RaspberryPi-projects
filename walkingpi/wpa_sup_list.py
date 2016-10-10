d8a96048da563e766d843dbf94bb7281029b4512
gary-dalton <gary@ggis.biz>
2016-10-10 08:50:14 -0500

    Double quotes to single quotes

--- walkingpi/wpa_sup_list.py
+++ walkingpi/wpa_sup_list.py
@@ -3,35 +3,34 @@ import errno
 import logging
 import time
 import shlex
-import argparse
 
 def run_command(the_call):
     """
-    Runs a program with arguments (e.g. rcmd="ls -lh /var/www")
-    Returns output if successful, or None and logs error if not.
+    Runs a program with arguments (the_call = 'wpa_cli -i wlan0 scan_result')
+    Returns output if successful and logs error if not.
     """
 
-    cmd = shlex.split(the_call)
-    executable = cmd[0]
-    executable_options = cmd[1:]
+    the_cmd = shlex.split(the_call)
+    executable = the_cmd[0]
+    executable_args = the_cmd[1:]
 
     try:
-        proc = Popen(([executable] + executable_options), stdout=PIPE, stderr=PIPE)
+        proc = Popen(([executable] + executable_args), stdout=PIPE, stderr=PIPE)
         response = proc.communicate()
         response_stdout, response_stderr = response[0], response[1]
     except OSError as e:
         if e.errno == errno.ENOENT:
-            logging.debug("Unable to locate '%s' program. Is it in your path?" % executable)
+            logging.debug('Unable to locate \'%s\'.' % executable)
         else:
-            logging.error("O/S error occured when trying to run '%s': \"%s\"" % (executable, str(e)))
+            logging.error('O/S error when trying to run \'%s\': "%s"' % (executable, str(e)))
     except ValueError as e:
-        logging.debug("Value error occured. Check your parameters.")
+        logging.debug("Value error occured.")
     else:
         if proc.wait() != 0:
-            logging.debug("Executable '%s' returned with the error: \"%s\"" % (executable, response_stderr))
+            logging.debug('Executable \'%s\' returned with the error: "%s"' % (executable, response_stderr))
             return response
         else:
-            logging.debug("Executable '%s' returned successfully. First line of response was \"%s\"" % (
+            logging.debug('Executable \'%s\' returned successfully. First line of response was "%s"' % (
             executable, response_stdout.split('\n')[0]))
             return response_stdout
 
@@ -40,19 +39,19 @@ def get_networks(iface, retry=10):
     Grab a list of wireless networks within range, and return a list of dicts describing them.
     """
     while retry > 0:
-        if "OK" in run_command("wpa_cli -i %s scan" % iface):
+        if 'OK' in run_command('wpa_cli -i %s scan' % iface):
             networks = []
-            r = run_command("wpa_cli -i %s scan_result" % iface).strip()
-            if "bssid" in r and len(r.split("\n")) > 1:
+            r = run_command('wpa_cli -i %s scan_result' % iface).strip()
+            if 'bssid' in r and len(r.split('\n')) > 1:
                 for line in r.split("\n")[1:]:
                     b, fr, s, f = line.split()[:4]
-                    ss = " ".join(line.split()[4:])  # Hmm, dirty
-                    networks.append({"bssid": b, "freq": fr, "sig": s, "ssid": ss, "flag": f})
+                    ss = " ".join(line.split()[4:])
+                    networks.append({'bssid': b, 'freq': fr, 'sig': s, 'ssid': ss, 'flag': f})
                 return networks
         retry -= 1
-        logging.debug("Couldn't retrieve networks, retrying")
+        logging.debug('Unable to retrieve networks, retrying')
         time.sleep(0.5)
-    logging.error("Failed to list networks")
+    logging.error('Failed to list networks')
 
 
 # Configure logging
@@ -67,15 +66,5 @@ if networks:
     for network in networks:
         print(network)
         logging.info('SSID:%s, Signal:%s, BSSID:%s, Security:%s, Freq:%s', network['ssid'], network['sig'], network['bssid'], network['flag'], network['freq'])
-        print(" SSID:%s" % network['ssid'])
-        print(" Signal:\t%s" % network['sig'])
-        print(" BSSID:\t%s" % network['bssid'])
-        print(" Security:\t%s" % network['flag'])
-        print(" Freq:\t%s\n" % network['freq'])
 else:
-    print
-    "[W] No wireless networks detected :-("
-
-#wpa_cli -i wlan0 scan
-#wpa_cli -i wlan0 scan_result
-'%s before you %s', 'Look', 'leap!'
+    logging.info('No wireless networks detected')

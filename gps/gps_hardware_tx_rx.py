ce18550e682819cd65fc37cf3918fa516d7cf383
gary-dalton <gary@ggis.biz>
2016-11-05 16:26:27 -0500

    Get hardware revision and work with the gps serial device.

--- gps/gps_hardware_tx_rx.py
+++ gps/gps_hardware_tx_rx.py
@@ -5,10 +5,11 @@ Provide a library and a test script to interact with the
 MediaTek GPS Chipset MT3339.
 """
 
-import datetime, time
+import datetime, time, os
 import microstacknode.hardware.gps.l80gps as l80gps
+import rpi_utils
 
-gps = l80gps.L80GPS()
+gps = connect_serial_gps()
 
 def basic_nmea():
     """
@@ -18,26 +19,31 @@ def basic_nmea():
     """
     Time, date, position, course and speed data. Recommended Minimum Navigation Information.
     """
+    print("GPMRC")
     print(gps.get_gprmc())
 
     """
     Time, position and fix type data.
     """
+    print("GPGGA")
     print(gps.get_gpgga())
 
     """
     GPS receiver operating mode, active satellites used in the position solution and DOP values.
     """
+    print("GPGSA")
     print(gps.get_gpgsa())
 
     """
     The number of GPS satellites in view satellite ID numbers, elevation, azimuth, and SNR values.
     """
+    print("GPGSV")
     print(gps.get_gpgsv())
 
     """
     The UTC date and time
     """
+    print("GPZDA")
     print(get_gpzda(gps))
 
 def gps_output_spy(duration_seconds=10):
@@ -126,3 +132,46 @@ def location_logging_test(gps, duration_minutes=5):
 
     return (locus_data)
 
+def get_device_gps():
+    """
+    Use the hardware revision to determine the serial device used by UART GPS.
+    See http://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/
+    :return: device string
+    """
+    pi1 = ['0002','0003','0004','0005','0006','0007','0008','0009','000d','000e','000f','0010','0011','0012','0013']
+    pi2 = ['a01041','a21041']
+    pi0 = ['900092']
+    pi3 = ['a02082','a22082']
+    rpi_revision = rpi_utils.get_revision()
+    if rpi_revision in pi1 or rpi_revision in pi2 or rpi_revision in pi0:
+        # Pi 2 or lower
+        device = '/dev/ttyAMA0'
+    else:
+        # Pi 3 or higher
+        device = '/dev/ttyS0'
+    return device
+
+def disconnect_serial_gps():
+    """
+    Remove the python gps serial connection and reattach gpsd to the device.
+    :return:
+    """
+    global gps
+    del gps
+    device = get_device_gps()
+    cmd = "sudo gpsd " + device + " -F /var/run/gpsd.sock"
+    os.system(cmd)
+    #rpi_utils.run_program(cmd)
+
+def connect_serial_gps():
+    """
+    Start by disconnecting any currently running gpsd using killall.
+    Then attach gps to the L80GPS object
+    :return: L80GPS object
+    """
+    cmd = "sudo killall gpsd"
+    os.system(cmd)
+    #rpi_utils.run_program(cmd)
+    time.sleep(2)
+    gps = l80gps.L80GPS()
+    return gps
--- rpi_utils.py
+++ rpi_utils.py
@@ -15,17 +15,16 @@ def run_program(rcmd):
     cmd = shlex.split(rcmd)
     executable = cmd[0]
     executable_options = cmd[1:]
-
     try:
         proc = Popen(([executable] + executable_options), stdout=PIPE, stderr=PIPE)
         response = proc.communicate()
         response_stdout, response_stderr = response[0], response[1]
-    except OSError, e:
+    except OSError as e:
         if e.errno == errno.ENOENT:
             logging.debug("Unable to locate '%s' program. Is it in your path?" % executable)
         else:
             logging.error("O/S error occured when trying to run '%s': \"%s\"" % (executable, str(e)))
-    except ValueError, e:
+    except ValueError as e:
         logging.debug("Value error occured. Check your parameters.")
     else:
         if proc.wait() != 0:
@@ -35,3 +34,22 @@ def run_program(rcmd):
             logging.debug("Executable '%s' returned successfully. First line of response was \"%s\"" % (
             executable, response_stdout.split('\n')[0]))
             return response_stdout
+
+def get_revision():
+    """
+    Extract board revision from cpuinfo file
+    from http://www.raspberrypi-spy.co.uk/2012/09/getting-your-raspberry-pi-revision-number-using-python/
+    :return: revision string
+    """
+    myrevision = "0000"
+    try:
+        f = open('/proc/cpuinfo', 'r')
+        for line in f:
+            if line[0:8] == 'Revision':
+                length = len(line)
+                myrevision = line[11:length - 1]
+        f.close()
+    except:
+        myrevision = "0000"
+    return myrevision
+
